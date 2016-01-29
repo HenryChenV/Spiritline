8aaf55350c551ddbfc91e4d5bf3d0916a0faaee7
Henry Chen <chen_hanli@163.com>
2016-01-30 02:17:16 +0800

    first commit
    Signed-off-by: Henry Chen <chen_hanli@163.com>

---
+++ pelicanconf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'henry'
+SITENAME = u'SpiritLine'
+SITEURL = 'http://localhost:8000'
+DISQUS_SITENAME = "spiritline"
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh_CN'
+DEFAULT_DATE_FORMAT = ('%Y-%m-%d (%A) %H:%M:%S')
+
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'hide'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('ThreeBook', 'http://threebook.cn'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+MD_EXTENSIONS = [
+    "extra",
+    "toc",
+    "headerid",
+    "meta",
+    "sane_lists",
+    "smarty",
+    "wikilinks",
+    "admonition",
+    "codehilite(guess_lang=False,pygments_style=emacs,noclasses=True)"
+]
+
+CNZZ_ANALYTICS = True
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+THEME = "themes/new-bootstrap2"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
