f96f1d8e57ab36ff537a58cfcab841d00a60e5b7
Henry Chen <chen_hanli@163.com>
2016-02-02 01:02:15 +0800

    update
    Signed-off-by: Henry Chen <chen_hanli@163.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -65,23 +65,70 @@ YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
 
 # Theme
-#THEME = "themes/new-bootstrap2"
-THEME = "themes/new-foundation-default-colours"
-#THEME = "themes/pelican-bootstrap3"
+#THEME = "themes/new-foundation-default-colours"
+THEME = "themes/pelican-bootstrap3"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 # Plugins
 PLUGIN_PATHS = ["plugins", ]
-PLUGINS = ["better_codeblock_line_numbering"]
+PLUGINS = ["better_codeblock_line_numbering", ]
 
 # Static Files
 STATIC_PATHS = ['images']
 
 DISPLAY_TAGS_ON_SIDEBAR = True
 DISPLAY_TAGS_INLINE = True
-
 DISPLAY_BREADCRUMBS = True
-
 SUMMARY_MAX_LENGTH = 15
+
+###################### for theme plumage #########################
+#LEFT_SIDEBAR = "left sidebar"
+#MENUITEMS = (
+#    ("item1", "#"),
+#    ("item2", "#"),
+#)
+#SITESUBTITLE = "site subtitle"
+#TIPUE_SEARCH = True
+###################### end #######################################
+
+
+###################### SIDEBAR = "sidebar.html" #########################
+#SITESUBTITLE ='Sub-title that goes underneath site name in jumbotron.'
+#SITETAG = "Text that's displayed in the title on the home page."
+
+# Extra stylesheets, for bootstrap overrides or additional styling.
+#STYLESHEET_FILES = ("pygment.css", "voidybootstrap.css",)
+
+# Put taglist at end of articles, and use the default
+# sharing button implementation.
+#CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
+#CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
+
+# Default sidebar template. Omit
+# this setting for single column
+# mode without sidebar.
+#SIDEBAR = "sidebar.html"
+#########################################################################
+
+########################## for theme chameleon ###########################
+#AUTHORS = {
+#    u'jack': '/about.html',
+#    u'mary': 'http://mary.info',
+#    u'tony': 'http://tony.me',
+#}
+#MENUITEMS = [
+#    ('Home', '/'),
+#    ('Archives', [
+#        ('Tags', '/tags.html'),
+#        ('Categories', '/categories.html'),
+#        ('Chronological', '/archives.html'),
+#    ]),
+#    ('Social', [
+#        ('Email', 'mailto: maurelinus@stoic.edu'),
+#        ('Github', 'http://url-to-github-page'),
+#        ('Facebook', 'http://url-to-facebook-page'),
+#    ]),
+#]
+#########################################################################
---
+++ themes/README.md
@@ -0,0 +1,26 @@
+# bootstrap2
+感觉不错的主题
+
+# chameleon
+归档不错，整体方方的，感觉文章显示有点窄
+
+# gum
+简单
+
+# new-bootstrap2
+样式好看，只是文章显示笔记窄
+
+# new-foundation-default-colours
+自己根据别人主题改的，
+下了点心思，调整了归档和其他一些小地方
+代码也能显示行号
+
+# new-voidy-bootstrap
+可定制性强，但归档很丑,
+tag不错
+
+# octopress
+大气好看，但功能简单，可能还需要发掘吧
+
+# pelican-bootstrap3
+就是样子好看，很多地方需要改
