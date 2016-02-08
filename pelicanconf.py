77526d94c3279aefdd4901f596093551351192be
Henry Chen <chen_hanli@163.com>
2016-02-08 16:43:48 +0800

    add ignore_files, delete template.md
    Signed-off-by: Henry Chen <chen_hanli@163.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,8 @@ DEFAULT_LANG = u'zh_CN'
 DEFAULT_DATE_FORMAT = ('%Y-%m-%d')
 
 USE_FOLDER_AS_CATEGORY = True
-DEFAULT_CATEGORY = 'hide'
+DEFAULT_CATEGORY = 'Essay'
+IGNORE_FILES = ['template.md']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
